5da28704cd7e5e3da99aeddc80eaa452031dd076
Akshay-Kibbcom <akshay.kashyap@kibbcom.in>
2018-12-27 10:48:47 +0530

    Mongo Test

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
 var MongoClient= require('mongodb').MongoClient;
 var url = 'mongodb://localhost/EmployeeDB';
-MongoClient.connect(url,function(err,db){
-    var cursor = db.collection('Employee').find();
 
-    cursor.each(function(err, doc) {
-
-        console.log(doc);
-
-    });
+app.get('/', function(req, res){
+    MongoClient.connect(url,function(err,db){
+        if(err){
+            res.send('Error connecting to Mongo');
+        }
+        res.send('Connected to Mongo')
+        db.close();
+    }) 
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
