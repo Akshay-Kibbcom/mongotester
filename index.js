3888f9c5aeedf12809802a26af141b0c2c8876dc
Akshay-Kibbcom <akshay.kashyap@kibbcom.in>
2018-12-27 12:21:59 +0530

    Mongo Test Kubes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var host= '100.65.180.68';
 
 var MongoClient= require('mongodb').MongoClient;
-var url = 'mongodb://localhost/EmployeeDB';
+var url = 'mongodb://'+host+'/EmployeeDB';
 
 app.get('/', function(req, res){
     MongoClient.connect(url,function(err,db){
