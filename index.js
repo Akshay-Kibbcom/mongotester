b88054179459d7dcab181d0863bffae66f5bd5cf
Akshay-Kibbcom <akshay.kashyap@kibbcom.in>
2019-01-09 15:35:09 +0530

    Check Jenkins 1

--- index.js
+++ index.js
@@ -9,18 +9,10 @@ var url = 'mongodb://'+host+'/EmployeeDB';
 app.get('/', function(req, res){
     MongoClient.connect(url,function(err,db){
         if(err){
-            res.send('Error connecting to Mongo');
+            res.send("error");
         }
-        
+        res.send("Success")
         console.log('Connected to Mongo')
-        db.collection('Employee').insertOne({
-            Employeeid: 1,
-            EmployeeName: "NewEmployee"
-        });
-        res.send('Successfully Wrote to Mongo');
-        console.log("Jenkings Testing");
-
-        db.close();
     }) 
 })
 
