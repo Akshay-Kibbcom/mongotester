4b88f91fb37f8cc9e7176449513e9db7c5a7fbc6
Akshay-Kibbcom <akshay.kashyap@kibbcom.in>
2019-01-10 15:03:42 +0530

    Mongo Tester 6

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
             Employeeid: 1,
             EmployeeName: "NewEmployee"
         });
-        res.send('Successfully Wrote to Mong o');
+        res.send('Successfully Wrote to Mon    g o');
 
 
         db.close();
